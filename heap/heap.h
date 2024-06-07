e211bbfed7bbdb26d394596d26c6740a0cea6863
KseniaKhalameidik <ksenon789@gmail.com>
2024-06-07 15:35:00 +1000

    add basic heap

---
+++ heap/heap.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+
+class heap {
+    std::vector<int> tree;
+
+    void sift_up(int i);
+    void sift_down(int i);
+
+public:
+    heap(const std::vector<int>& array) : tree(array) {
+        // make heapify
+        for(int j=tree.size()/2; j>=0; j--){
+            sift_down(j);
+        }
+    }
+};
---
+++ heap/tests.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "heap.h"
+
+void test_remove_from_empty() {
+    heap h({1, 2, 300});
+}
+void test_add_one_value_and_print() {}
+void test_add_random_values_and_print() {}
+void test_add_and_remove_random_values_and_print() {}
